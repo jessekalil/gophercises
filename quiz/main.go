38489118c7a159e9d8eac3c8ba603c6b547dc6e3
Jesse Kalil Martins <jessekalyl1491@gmail.com>
2021-08-29 18:07:33 -0300

    Quiz

--- quiz/main.go
+++ quiz/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -15,6 +16,7 @@ type problem struct {
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 5, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -29,19 +31,32 @@ func main() {
 	}
 
 	problems := parseLines(lines)
-	fmt.Println(problems)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 	correct := 0
+
+problemLoop:
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerChan := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChan <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			break problemLoop
+		case answer := <-answerChan:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+	fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+
 }
 
 func parseLines(lines [][]string) []problem {
